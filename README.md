76659c317d6b85722f6ce4cb642f7d3bc2aa447b
José Wilker <jose.wilker@smartapps.com.br>
2015-05-21 14:24:05 -0300

    added test for mraa

---
+++ README.md
@@ -0,0 +1,5 @@
+THIS IS ATHENA PROJECT!
+
+Athena is like a virtual assistent.
+
+created by José Wilker <wilker@wilker.com.br>
--- init.js
+++ init.js
@@ -1,1 +1,6 @@
 console.log("hello World!!!");
+var m = require('mraa');
+var i2c = new m.I2c(1);
+//i2c.address(0x63);
+i2c.write("R,56.26");
+console.log("Reading I2C..");
