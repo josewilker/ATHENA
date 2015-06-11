5363bfb278e65d0c9d4d659e1e2bac554147cff5
José Wilker <jose.wilker@smartapps.com.br>
2015-06-11 03:07:48 -0300

    doing test

--- libs/wdactions.js
+++ libs/wdactions.js
@@ -6,7 +6,7 @@ wdactions.io = {};
 wdactions.load = function() {
 
     objIO = wdactions.io;
-    files = ['ip.js', 'mtemp.js', 'songs.js'];
+    files = ['greetings.js','ip.js', 'mtemp.js', 'songs.js'];
     arrayFilesLength = Object.keys(_settingsConfig.actions).length;
     for(ari=0; ari < arrayFilesLength; ari++) {
         freplace = files[ari].replace(".js","");
