d01f0b341e4e4c467551f924b429b10c40360897
José Wilker <jose.wilker@smartapps.com.br>
2015-06-18 02:01:52 -0300

    solved problem with actions

--- libs/wdactions.js
+++ libs/wdactions.js
@@ -5,6 +5,7 @@ wdactions = {};
 wdactions.io = {};
 
 wdactions.getAll = function(path) {
+    //wdactions.getAll("./actions/");
     return fs.readdirSync(path).filter(function(file) {
         return fs.statSync(path.join(path, file)).isDirectory();
     });
@@ -13,11 +14,10 @@ wdactions.getAll = function(path) {
 wdactions.load = function() {
 
     objIO = wdactions.io;
-    files = wdactions.getAll("./actions/"); //['greetings.js','ip.js', 'mtemp.js', 'songs.js', 'flow.js', 'lights.js', 'didact.js'];
+    files = ['greetings.js','ip.js', 'mtemp.js', 'songs.js', 'flow.js', 'lights.js', 'didact.js'];
     arrayFilesLength = Object.keys(_settingsConfig.actions).length;
     for(ari=0; ari < arrayFilesLength; ari++) {
-
-        freplace = files[ari]; // files[ari].replace(".js","");
+        freplace = files[ari].replace(".js","");
 
         eval("objIO." + freplace + "={};");
         eval("objIO." + freplace + ".obj=require('../actions/" + freplace + "/wd.js');");
